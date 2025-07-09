d16ca9bee5390d67971e1dc02de9c0570619d728
AratiDabhole <aratidabhole5020@gmail.com>
2025-07-09 15:14:53 +0530

    My First

--- src/basicprogram/Hello.java
+++ src/basicprogram/Hello.java
@@ -3,9 +3,8 @@ package basicprogram;
 public class Hello {
 
 	public static void main(String[] args) {
-int a=10,b1=90;
-System.out.println(a);
-System.out.println(b1);
+		int a = 10, b1 = 90;
+		System.out.println(a);
 
 	}
 
